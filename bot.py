c7ab03bb424e2bfbd1d9f023bd6a65606007a113
Elijah <business@elijahpepe.com>
2022-03-17 07:38:17 -0700

    Add custom wordlist argument

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ range_list = list(range(0, 16))
 character_list = list(string.ascii_lowercase)
 number_list = list(range(0, 99))
 word_list = open("wordlist.txt","r").read().split("\n")
+args = sys.argv
 
 emails = []
 
@@ -41,6 +42,10 @@ for x in generate("abcdefghijklmnopqrstuvwxyz"):
 
 for x in number_list:
 		test(x)
+
+if args[0]:
+		for x in open(args[0],"r").read().split("\n")
+			test(x)
 
 with open('emails.txt', 'w') as f:
     for item in emails:
