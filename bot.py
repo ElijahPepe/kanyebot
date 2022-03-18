05ee84a9a586a97cd455a7773699081b12215925
ElijahPepe <business@elijahpepe.com>
2022-03-17 17:13:07 -0700

    Refactor

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from colorama import Fore, Style
 import requests
 import time
 import base64
@@ -18,8 +19,8 @@ def generate(l):
 		yield from itertools.product(*([l] * num_item[0]))
 
 def test(test_email):
-		time.sleep(.5)
-		email = test_email + "@gmail.com"
+		time.sleep(.25)
+		email = str(test_email) + "@gmail.com"
 		timestamp = round(int(time.time()))
 		authorization = email + ':' + str(timestamp)
 		bytes = authorization.encode("utf-8")
@@ -34,20 +35,52 @@ def test(test_email):
 		if r.status_code == 200:
 			print(email)
 			emails.append(email)
+		else:
+			print(Style.DIM + email + Style.RESET_ALL)
 
-for x in word_list:
-		test(x)
+def logo():
+	return """
+  _  __                      _           _   
+ | |/ /                     | |         | |  
+ | ' / __ _ _ __  _   _  ___| |__   ___ | |_ 
+ |  < / _` | '_ \| | | |/ _ \ '_ \ / _ \| __|
+ | . \ (_| | | | | |_| |  __/ |_) | (_) | |_ 
+ |_|\_\__,_|_| |_|\__, |\___|_.__/ \___/ \__|
+                   __/ |                     
+                  |___/                      
+v%s
+	""" % "1.0.0"
 
-for x in generate("abcdefghijklmnopqrstuvwxyz"):
-		test(''.join(x))
+def header():
+	line = "Emails: "
+	count = len(number_list) + len(word_list)
 
-for x in number_list:
-		test(x)
+	if args[0]:
+		count = count + len(open(args[0],"r").read().split("\n"))
 
-if args[0]:
-		for x in open(args[0],"r").read().split("\n"):
+	return Style.BRIGHT + Fore.YELLOW + line + Fore.CYAN + str(count) + "+" + Style.RESET_ALL
+
+def main():
+	print(logo())
+
+	print(header())
+	print(Style.RESET_ALL)
+
+	for x in word_list:
 			test(x)
 
-with open('emails.txt', 'w') as f:
-    for item in emails:
-        f.write("%s\n" % item)
+	for x in number_list:
+			test(x)
+
+	if args[0]:
+			for x in open(args[0],"r").read().split("\n"):
+				test(x)
+
+	for x in generate("abcdefghijklmnopqrstuvwxyz"):
+			test(''.join(x))
+
+	with open('emails.txt', 'w') as f:
+  	  for item in emails:
+    	    f.write("%s\n" % item)
+
+main()
